09a75bebfcd17bee2f095b7cf3551b650ad1d96f
MUHAMMADSHOAIBB <99481729+MUHAMMADSHOAIBB@users.noreply.github.com>
2022-04-03 10:11:10 +0500

    Update 11.4.py

--- 11.4.py
+++ 11.4.py
@@ -2,9 +2,6 @@ import random
 print('======================Task 11.4=================')
 names = ['yasir','hamad','abdulmuhaimin','rehan','nazal','ali','farhan']
 for x in names:
-        h=0
-        while h <=7:
-         random.seed(h)
-         score = random.randrange(60,90,5)
-         print(x, "your score is",score)
-         h =h+1
+        random.seed(0)
+        score = random.randint(60,90)
+        print(x, "your score is",score)
